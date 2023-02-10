2c2e638c1d4286f90250308a6be9cb666a6db61e
dumi <Dumi Loghin>
2023-02-10 19:27:38 +0800

    clean microbench folder

---
+++ microbench/verify/verify-cpu-sm2.go
@@ -0,0 +1,153 @@
+/*
+ * Copyright 2022 Dumitrel Loghin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"hash"
+	"os"
+	"strings"
+	"sync"
+	"time"
+	"crypto/rand"
+	"strconv"
+
+	"github.com/tjfoc/gmsm/sm2"
+
+	"go.uber.org/atomic"
+	"golang.org/x/crypto/sha3"
+	"gopkg.in/alecthomas/kingpin.v2"
+
+	"hbdb/src/benchmark"	
+)
+
+type Pair struct {
+	a, b []byte
+}
+
+var (
+	dataRun       = kingpin.Flag("run-path", "Path of YCSB operation data").Required().String()	
+	concurrency   = kingpin.Flag("nthreads", "Number of threads for each driver").Default("10").Int()
+	saveResults   = kingpin.Flag("save", "Save digests to output-cpu.txt").Bool()
+)
+
+type KeccakState interface {
+	hash.Hash
+	Read([]byte) (int, error)
+}
+
+func myKeccak256(data ...[]byte) []byte {
+	b := make([]byte, 32)
+	d := sha3.New256().(KeccakState)
+	for _, b := range data {
+		d.Write(b)
+	}
+	d.Read(b)
+	return b
+}
+
+func mySm2Sign(priv *sm2.PrivateKey, msg []byte) ([]byte, error) {
+	return priv.Sign(rand.Reader, msg, nil)	
+}
+
+func mySm2Verify(pub *sm2.PublicKey, msg, sig []byte) bool {
+	return pub.Verify(msg, sig)
+}
+
+func main() {
+	kingpin.Parse()
+
+	// generate key
+	pvk, err := sm2.GenerateKey(rand.Reader)
+	if err != nil {
+		panic(err)
+	}
+	pub := &pvk.PublicKey	
+
+	fmt.Println("Load data ...")
+	runFile, err := os.Open(*dataRun)
+	if err != nil {
+		panic(err)
+	}
+	defer runFile.Close()
+	runBuf := make(chan Pair, 100000)
+	reqNum := atomic.NewInt64(0)
+	wg := &sync.WaitGroup{}	
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		defer close(runBuf)
+		if err := benchmark.LineByLine(runFile, func(line string) error {
+			reqNum.Add(1)
+			operands := strings.SplitN(line, " ", 5)
+			l := len(operands[2])
+			// if l%8 != 0 {
+			//	l = 8 * (l/8 + 1)
+			// }
+			// copy data
+			buf := make([]byte, l)
+			copy(buf, operands[2])
+			msg := myKeccak256(buf)
+			sig, err := mySm2Sign(pvk, msg)
+			if err != nil {
+				fmt.Println(err)
+			}
+			runBuf <- Pair{buf, sig}
+			return nil
+		}); err != nil {
+			panic(err)
+		}
+	}()
+	time.Sleep(5 * time.Second)
+
+	fmt.Println("Start running ...")
+	if *saveResults {
+		outFile, err := os.Create("output-cpu.txt")
+		if err != nil {
+			fmt.Printf("Error creating output file: %v\n", err)
+		}
+		for pair := range runBuf {
+			dig := myKeccak256(pair.a)
+			res := "0"
+			if mySm2Verify(pub, dig, pair.b) {
+				res = "1"
+			}
+			// outFile.WriteString("|" + msg + "|\n")
+			// outFile.WriteString(hex.EncodeToString(hash) + " " + strconv.FormatInt(int64(len(msg)), 10) + "\n")
+			outFile.WriteString(hex.EncodeToString(dig) + " " + res + " " + hex.EncodeToString(pair.b) + " " + strconv.FormatInt(int64(len(pair.b)), 10) + "\n")
+		}
+		outFile.Close()
+	} else {
+		start := time.Now()
+		for j := 0; j < *concurrency; j++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				for pair := range runBuf {
+					dig := myKeccak256(pair.a)
+					mySm2Verify(pub, dig, pair.b[:64])
+				}
+			}()
+		}
+		wg.Wait()
+		delta := time.Since(start).Seconds()
+		fmt.Printf("Throughput with %v concurrency to handle %v requests: %v req/s\n",
+			*concurrency, reqNum, int64(float64(reqNum.Load())/delta),
+		)
+	}
+}
--- microbench/verify/verify-gpu-sm2.go
+++ microbench/verify/verify-gpu-sm2.go
@@ -17,27 +17,27 @@
 package main
 
 // #cgo CFLAGS: -I/home/dumi/git/RapidEC/src
-// #cgo LDFLAGS: -L/home/dumi/git/RapidEC/src -lverify
+// #cgo LDFLAGS: -L/home/dumi/git/RapidEC/src -lverify -lgomp -lgmp
 // #include "hbdb_interface.h"
 import "C"
 
 import (
 	"fmt"
 	"hash"
+	"encoding/hex"
 	"os"
 	"strings"
 	"sync"
 	"time"
 	"unsafe"
+	"crypto/rand"
+	"github.com/tjfoc/gmsm/sm2"
 
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/crypto/secp256k1"
 	"go.uber.org/atomic"
 	"golang.org/x/crypto/sha3"
 	"gopkg.in/alecthomas/kingpin.v2"
 
-	"hbdb/src/benchmark"
-	"hbdb/src/utils"
+	"hbdb/src/benchmark"	
 )
 
 type Pair struct {
@@ -45,9 +45,7 @@ type Pair struct {
 }
 
 var (
-	dataRun       = kingpin.Flag("run-path", "Path of YCSB operation data").Required().String()
-	keyFilePrefix = kingpin.Flag("key-file", "Prefix of Key Files").Required().String()
-	concurrency   = kingpin.Flag("nthreads", "Number of threads for each driver").Default("10").Int()
+	dataRun       = kingpin.Flag("run-path", "Path of YCSB operation data").Required().String()	
 	saveResults   = kingpin.Flag("save", "Save digests to output-cpu.txt").Bool()
 )
 
@@ -66,23 +64,36 @@ func myKeccak256(data ...[]byte) []byte {
 	return b
 }
 
+func mySm2Sign(priv *sm2.PrivateKey, msg []byte) ([]byte, error) {
+	return priv.Sign(rand.Reader, msg, nil)	
+}
+
+func mySm2SignRS(priv *sm2.PrivateKey, msg []byte) ([]byte, error) {
+	sig, _ := priv.Sign(rand.Reader, msg, nil)
+	ret := make([]byte, 64)
+	lr := int(sig[3])
+	copy(ret[:32], sig[4:4+lr])	
+	copy(ret[32:], sig[6+lr:])
+	return ret, nil
+}
+
+func mySm2Verify(pub *sm2.PublicKey, msg, sig []byte) bool {
+	return pub.Verify(msg, sig)
+}
+
 func main() {
 	kingpin.Parse()
 
-	// load private key
-	fmt.Println("Load keys ...")
-	pvk, err := crypto.LoadECDSA(*keyFilePrefix + ".pvk")
+	// generate key
+	pvk, err := sm2.GenerateKey(rand.Reader)
 	if err != nil {
 		panic(err)
 	}
+	pub := &pvk.PublicKey
 
-	pub, err := utils.LoadECDSAPub(*keyFilePrefix + ".pub")
-	if err != nil {
-		panic(err)
-	}
-	pvk.PublicKey = *pub
-
-	pubkey, privkey := utils.DecodeKeyPair(pvk)
+	pubkey := make([]byte, 64)
+	copy(pubkey[:32], pub.X.Bytes())
+	copy(pubkey[32:], pub.Y.Bytes())
 
 	fmt.Println("Load data ...")
 	runFile, err := os.Open(*dataRun)
@@ -108,7 +119,7 @@ func main() {
 			buf := make([]byte, l)
 			copy(buf, operands[2])
 			msg := myKeccak256(buf)
-			sig, err := secp256k1.Sign(msg, privkey)
+			sig, err := mySm2SignRS(pvk, msg)
 			if err != nil {
 				fmt.Println(err)
 			}
@@ -128,8 +139,9 @@ func main() {
 		}
 	}
 
-	// init kernel
-	num := 100
+	// Run on GPU
+	fmt.Println("Run on GPU ...")
+	num := 32
 	cnum := C.int(num)
 	C.init_gpu(cnum)
 	data := make([]byte, num*256)
@@ -159,14 +171,15 @@ func main() {
 			gptr := (*C.uchar)(unsafe.Pointer(&signatures[0]))
 			// fmt.Printf("Size %v Offset %d\n", size, offset)
 			C.run_kernel(dptr, sptr, kptr, gptr, cnum)
-			var rptr *C.int
-			rptr = C.get_results(cnum)
 			if *saveResults {
+				// var rptr *C.int
+				res := C.GoBytes(unsafe.Pointer(C.get_results(cnum)), cnum)
+				hashes := C.GoBytes(unsafe.Pointer(C.get_hashes(cnum)), 3200)
 				for i := 0; i < num; i++ {
-					if rptr[i] == 1 {
-						outFile.WriteString("1\n")
+					if res[i] == 1 {						
+						outFile.WriteString(hex.EncodeToString(hashes[32*i:32*i+32]) + " 1\n")
 					} else {
-						outFile.WriteString("0\n")
+						outFile.WriteString(hex.EncodeToString(hashes[32*i:32*i+32]) + " 0\n")
 					}
 				}
 			}
@@ -177,8 +190,8 @@ func main() {
 		}
 	}
 	delta := time.Since(start).Seconds()
-	fmt.Printf("Throughput with %v concurrency to handle %v requests: %v req/s\n",
-		*concurrency, reqNum, int64(float64(reqNum.Load())/delta),
+	fmt.Printf("Throughput on GPU to handle %v requests: %v req/s\n",
+		reqNum, int64(float64(reqNum.Load())/delta),
 	)
 	if *saveResults {
 		outFile.Close()
